39d9ca3589a9a305570c8a251dba9ceda18c7060
Austin <austin@localhost.localdomain>
2012-12-09 20:44:13 -0500

    formatting

--- src/main/java/com/soundbite/gcharts/ColumnType.java
+++ src/main/java/com/soundbite/gcharts/ColumnType.java
@@ -1,20 +1,20 @@
 package com.soundbite.gcharts;
 
 public enum ColumnType {
-        STRING("string"),
+	STRING("string"),
 	NUMBER("number"),
-        DATE("date");
-        // TODO
-        //	BOOLEAN("boolean"),
-        //	DATE_TIME("datetime"),
-        //	TIME_OF_DAY("timeofday");
-	
+	DATE("date");
+	// TODO
+	//	BOOLEAN("boolean"),
+	//	DATE_TIME("datetime"),
+	//	TIME_OF_DAY("timeofday");
+
 	private final String string_value;
-	
+
 	private ColumnType(String s) {
 		string_value = s;
 	}
-	
+
 	@Override
 	public String toString() {
 		return string_value;
