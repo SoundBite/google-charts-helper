756cd7e4017bca5bd2c789eedfd39737dd049627
Andrew Brampton <me@bramp.net>
2013-12-02 21:30:43 -0500

    Renamed the LitralBuilder test case.

--- src/test/java/com/soundbite/gcharts/test/TestBuilder.java
+++ src/test/java/com/soundbite/gcharts/test/TestBuilder.java
@@ -1,5 +1,6 @@
 package com.soundbite.gcharts.test;
 
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -10,22 +11,21 @@ import com.soundbite.gcharts.Cell;
 import com.soundbite.gcharts.ColumnType;
 import com.soundbite.gcharts.Builder;
 
-public class TestLiteralBuilder {
+public class TestBuilder {
 
 	@Test
-	public void test() {
-		
+	public void test() throws IOException {
+
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.DATE, -1);
-		
+
 		Builder builder = new Builder().addColumn(ColumnType.DATE, "my label").addColumn(ColumnType.NUMBER, "value"); 
 		Assert.assertNotNull(builder.build());
-		
+
 		builder.addRow(Cell.of(new Date()), Cell.of(4))
 			.addRow(Cell.of(cal.getTime()), Cell.of(2));
-		
+
 		System.out.println(builder.build());
-		
 		
 		Builder b2 = new Builder();
 		b2.addColumn(ColumnType.STRING).addColumn(ColumnType.NUMBER);
@@ -33,9 +33,9 @@ public class TestLiteralBuilder {
 		b2.addRow(Cell.of("qwer"), Cell.of(5));
 		System.out.println(b2.build());
 
-                System.out.println("No rows example");
-                Builder b3 = new Builder();
-                b3.addColumn(ColumnType.STRING,"stringcol").addColumn(ColumnType.NUMBER, "numberCol");
-                System.out.println(b3.build());
+        System.out.println("No rows example");
+        Builder b3 = new Builder();
+        b3.addColumn(ColumnType.STRING,"stringcol").addColumn(ColumnType.NUMBER, "numberCol");
+        System.out.println(b3.build());
 	}
 }
